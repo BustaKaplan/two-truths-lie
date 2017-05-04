1c2a9f348bf1c31016ef3c9625e0a60dd9f74e63
renestl <renestl@gmail.com>
2017-05-03 21:31:00 -0500

    basic functionality is complete

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.element').hover(
+		function() {
+			$(this).addClass("hover");
+		}, function() {
+			$(this).removeClass("hover");
+		}
+	);
+
+	$('.lie').on('click', function() {
+		$('.element').removeClass("truth1");
+		var lieText = "Wrong Answer"
+		$(this).addClass("lie1");
+		$('p').html(lieText).removeClass("greenText");
+		$('p').html(lieText).addClass("redText");
+
+	});
+
+	$('.truth').on('click', function() {
+		$('.element').removeClass("truth1 lie1");
+		var correctText = "Correct"
+		$(this).addClass("truth1");
+		$('p').html(correctText).removeClass("redText");
+		$('p').html(correctText).addClass("greenText");
+		
+		
+	})
+	$('button').on('click', function() {
+		
+	})
+
+});
