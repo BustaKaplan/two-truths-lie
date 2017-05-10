d41548c23cffe4f6c343f70a3b857648715f87fd
BustaKaplan <chelsea.kaplan3@gmail.com>
2017-05-10 18:51:53 -0500

    Chelsea's edits

--- main.js
+++ main.js
@@ -22,11 +22,15 @@ $(document).ready(function() {
 		$(this).addClass("truth1");
 		$('p').html(correctText).removeClass("redText");
 		$('p').html(correctText).addClass("greenText");
-		
-		
+
+
 	})
-	$('button').on('click', function() {
-		
-	})
+	//$('button').on('click', function() {})
+	$("button").on("click",function(){
+
+	    $(".cat").css("display","block")
+
+	});
+
 
 });
